d765debb80c8d5fbd95a651cefb7289c2cf61926
nwatts <nasirwatts@outlook.com>
2023-06-12 03:21:04 -0400

    bold move

--- contact-form-submission.php
+++ contact-form-submission.php
@@ -1,25 +1,21 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-  // Retrieve the form data
   $name = $_POST["name"];
   $email = $_POST["email"];
   $message = $_POST["message"];
 
-  // Perform form validation
   $errors = [];
 
-  // Validate name
   if (empty($name)) {
     $errors[] = "Name is required";
   }
 
-  // Validate email
   if (empty($email)) {
     $errors[] = "Email is required";
   } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     $errors[] = "Invalid email format";
   } else {
-    // Validate against known email domains
+
     $allowedDomains = [
       'gmail.com',
       'yahoo.com',
@@ -58,7 +54,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $errors[] = "Message is required";
   }
 
-  // If there are any validation errors, return the error messages
   if (!empty($errors)) {
     $response = [
       "success" => false,
@@ -69,17 +64,16 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     exit;
   }
 
-  // Send an email with the form data
   $to = "nasirwatts@outlook.com";
   $subject = "New contact form submission";
   $emailBody = "Name: $name\n";
   $emailBody .= "Email: $email\n";
   $emailBody .= "Message: $message\n";
 
-  // Send the email
+  
   $success = mail($to, $subject, $emailBody);
 
-  // Return a response to the client
+  
   if ($success) {
     $response = [
       "success" => true,
@@ -94,7 +88,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     echo json_encode($response);
   }
 } else {
-  // Invalid request method
+  
   $response = [
     "success" => false,
     "message" => "Invalid request method"
