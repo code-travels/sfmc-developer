7994acf7a8001399a9c44d2ba884f470f69dae2f
nwatts <nasirwatts@outlook.com>
2023-06-12 10:58:11 -0400

    made form validation visible

--- index.js
+++ index.js
@@ -16,21 +16,21 @@ function initMap() {
 
 
 // This is the JavaScript file for the email validation on the contact page.
-// document.getElementById('contactForm').addEventListener('submit', function(e) {
-//   var email = document.getElementById('FormControlInput2').value;
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  var email = document.getElementById('FormControlInput2').value;
 
   
 
   // A simple regular expression for basic email validation
-  // var regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  var regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
-  // if ("email".match(regex) === null) {
-  //   document.getElementById('emailError').textContent = 'Please enter a valid email address.';
-  //   e.preventDefault();
-  // } else if (email === '') {
-  //   document.getElementById('emailError').textContent = 'Please enter your email address.';
-  //   e.preventDefault();
-  // } else if (knownDomains.indexOf(email.split('@')[1]) === -1) {
-  //    document.getElementById('emailError').textContent = 'Please enter a valid email address.';
-  //   e.preventDefault();
-  // }});
+  if ("email".match(regex) === null) {
+    document.getElementById('emailError').textContent = 'Please enter a valid email address.';
+    e.preventDefault();
+  } else if (email === '') {
+    document.getElementById('emailError').textContent = 'Please enter your email address.';
+    e.preventDefault();
+  } else if (knownDomains.indexOf(email.split('@')[1]) === -1) {
+     document.getElementById('emailError').textContent = 'Please enter a valid email address.';
+    e.preventDefault();
+  }});
